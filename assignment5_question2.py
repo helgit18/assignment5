822b0ff14bbec6f6f7092c05e2b67f6b88571d76
Helgi Hrafn Þorsteinsson <Helgi-Hrafn@Helgis-MacBook-Pro.local>
2018-09-04 17:32:47 +0000

    first commit

---
+++ assignment5_question2.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+for i in range(1,n):
+    if i == 1:
+        print(i)
+    elif i == 2:
+        print(i)
+    elif i == 3:
+        print(i)
